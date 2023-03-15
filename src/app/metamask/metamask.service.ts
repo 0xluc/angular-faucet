eff897a56886ca48da0163d96103092520df6d18
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-15 00:19:54 -0300

    added metamask integration

--- src/app/metamask/metamask.service.ts
+++ src/app/metamask/metamask.service.ts
@@ -11,26 +11,25 @@ declare global {
   providedIn: 'root'
 })
 export class MetamaskService {
-  ethereum = window.ethereum;
-  web3: any
-  selectedAccount: string = ''
-  selectedNetwork: any
+  // ethereum = window.ethereum;
+  // web3: any
   avaxFujiTestnet: string = 'https://api.avax-test.network/ext/bc/C/rpc'
+  
 
+  ethereum = window.ethereum;
+  web3:any
 
-  constructor(private cd: ChangeDetectorRef) { }
-  connectToMetamask() {
+  constructor() { }
+  switchToAvax(selectedNetwork:any) {
     if(typeof this.ethereum === 'undefined'){
       console.log("Install metamask")
       return;
     }
-    this.web3 = new Web3(window.ethereum)
-    this.ethereum.request({ method: 'eth_requestAccounts' })
-      .then((accounts: any) => {
-        this.selectedAccount = accounts[0];
-        this.selectedNetwork = window.ethereum.networkVersion;
-
-        if (this.selectedNetwork !== '43113') {
+     this.web3 = new Web3(window.ethereum)
+     this.ethereum.request({ method: 'eth_requestAccounts' })
+      .then(() => {
+        selectedNetwork = window.ethereum.networkVersion;
+         if (selectedNetwork !== '43113') {
             if (!window.ethereum.networkVersion || window.ethereum.networkVersion !== '43113') {
           // Add the AVAX Fuji Testnet if it's not already added
           window.ethereum.request({ method: 'wallet_addEthereumChain', params: [{ 
@@ -43,13 +42,13 @@ export class MetamaskService {
             },
             rpcUrls: [this.avaxFujiTestnet],
             blockExplorerUrls: ['https://cchain.explorer.avax-test.network']
-          }] })
-           .then(() => {
+          }] 
+        })
+                   .then(() => {
               // Switch to the AVAX Fuji Testnet
               window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0xa869' }] })
                 .then(() => {
-                  this.selectedNetwork = '0xa869';
-                  this.cd.detectChanges();
+                  selectedNetwork = '0xa869';
                 })
                 .catch((err: any) => console.error(err));
             })
@@ -58,8 +57,7 @@ export class MetamaskService {
           // Switch to the AVAX Fuji Testnet
                 window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0xa869' }] })
             .then(() => {
-              this.selectedNetwork = '43113';
-              this.cd.detectChanges();
+              selectedNetwork = '43113';
             })
             .catch((err: any) => console.error(err));
         }
@@ -67,25 +65,180 @@ export class MetamaskService {
     })
       .catch((err: any) => console.error(err));
 
-    this.ethereum.on('accountsChanged', (accounts: any) => {
-      this.selectedAccount = accounts[0];
-      this.cd.detectChanges()
-    });
-    window.ethereum.on('chainChanged', (chainId: any) => {
-      if (chainId !== '43113') {
-      // Switch to the AVAX Fuji Testnet
-      window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0xa869' }] })
-        .then(() => {
-          this.selectedNetwork = '43113';
-          this.cd.detectChanges();
-        })
-        .catch((err: any) => console.error(err));
-    } else {
-      this.selectedNetwork = '43113';
-        this.cd.detectChanges();
-    }
+
+  }
+  
+  connectToMetamask(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if(!window.ethereum){
+          reject ('Metamask is not installed!')
+          return;
+        }
+        if (window.ethereum.networkVersion !== '43113') {
+          try {
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: '0xa869' }]
+            });
+          } catch (error:any) {
+            if (error.code === 4902) {
+              try {
+                await window.ethereum.request({
+                  method: 'wallet_addEthereumChain',
+                  params: [{
+                    chainId: '0xa869',
+                    chainName: 'Avalanche Fuji Testnet',
+                    nativeCurrency: {
+                      name: 'Avalanche',
+                      symbol: 'AVAX',
+                      decimals: 18
+                    },
+                    rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc'],
+                    blockExplorerUrls: ['https://cchain.explorer.avax-test.network/']
+                  }]
+                });
+              } catch (error) {
+                reject('Failed to connect to Avalanche Fuji Testnet');
+                return;
+              }
+            }else {
+              reject('Failed to switch to Avalanche Fuji Testnet');
+              return;
+            }
+          }
+        }
+         // Connect to wallet and retrieve account
+        try {
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          const address = accounts[0];
+          resolve(address);
+        } catch (error) {
+          reject('Failed to connect to wallet');
+        }
+      } catch (error:any) {
+        reject(error.message);
+      }
     });
   }
+  async onAccountsChanged(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const address = accounts[0];
+        resolve(address);
+      } catch (error) {
+        reject('Failed to retrieve account');
+      }
+    });
+  }
+  onNetworkChanged(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (window.ethereum.networkVersion === '43113') {
+          resolve('Avalanche Fuji Testnet');
+        } else {
+          reject('Incorrect network selected');
+        }
+        } catch (error:any) {
+        reject(error.message);
+      }
+    });
+  }
+   public addWalletChangeListener(callback: (accounts: string[]) => void): void {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', callback);
+    }
+  }
+  public removeWalletChangeListener(callback: (accounts: string[]) => void): void {
+    if (window.ethereum) {
+      window.ethereum.removeListener('accountsChanged', callback);
+    }
+  }
+  public addChainChangedListener(callback: (chainId: string) => void){
+    if(window.ethereum) {
+      window.ethereum.on('chainChanged', (chainId: string) => {
+        callback(chainId)
+      })
+    }
+  }
+  public removeChainChangedListener(callback: (chainId:string ) => void) {
+    if(window.ethereum) {
+      window.ethereum.removeListener('chainChanged', (chainId: string) => {
+        callback(chainId)
+      })  
+    }
+  }
+}
+
+//     }) 
+//     if(typeof this.ethereum === 'undefined'){
+//       reject("Install metamask")
+//       return;
+//     }
+//     this.web3 = new Web3(window.ethereum)
+//     this.ethereum.request({ method: 'eth_requestAccounts' })
+//       .then((accounts: any) => {
+//         this.selectedAccount = accounts[0];
+//         this.selectedNetwork = window.ethereum.networkVersion;
+
+//         if (this.selectedNetwork !== '43113') {
+//             if (!window.ethereum.networkVersion || window.ethereum.networkVersion !== '43113') {
+//           // Add the AVAX Fuji Testnet if it's not already added
+//           window.ethereum.request({ method: 'wallet_addEthereumChain', params: [{ 
+//             chainId: '0xa869',
+//             chainName: 'Avalanche Fuji Testnet',
+//             nativeCurrency: {
+//               name: 'AVAX',
+//               symbol: 'AVAX',
+//               decimals: 18
+//             },
+//             rpcUrls: [this.avaxFujiTestnet],
+//             blockExplorerUrls: ['https://cchain.explorer.avax-test.network']
+//           }] })
+//            .then(() => {
+//               // Switch to the AVAX Fuji Testnet
+//               window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0xa869' }] })
+//                 .then(() => {
+//                   this.selectedNetwork = '0xa869';
+//                   // should reload
+//                 })
+//                 .catch((err: any) => console.error(err));
+//             })
+//             .catch((err: any) => console.error(err));
+//         } else {
+//           // Switch to the AVAX Fuji Testnet
+//                 window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0xa869' }] })
+//             .then(() => {
+//               this.selectedNetwork = '43113';
+//               // should reload
+//             })
+//             .catch((err: any) => console.error(err));
+//         }
+//       }
+//     })
+//       .catch((err: any) => console.error(err));
+
+//     this.ethereum.on('accountsChanged', (accounts: any) => {
+//       this.selectedAccount = accounts[0];
+//       // should reload
+//     });
+//     window.ethereum.on('chainChanged', (chainId: any) => {
+//       if (chainId !== '43113') {
+//       // Switch to the AVAX Fuji Testnet
+//       window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0xa869' }] })
+//         .then(() => {
+//           this.selectedNetwork = '43113';
+//           // should reload
+
+//         })
+//         .catch((err: any) => console.error(err));
+//     } else {
+//       this.selectedNetwork = '43113';
+//         // should reload
+//     }
+//     });
+//   }
 
   
-}
+// }
--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { MetamaskService } from './../metamask/metamask.service';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.css']
+  styleUrls: ['./wallet.component.css'],
 })
-export class WalletComponent {
+export class WalletComponent implements OnInit {
+  public address: string = '';
+  public selectedNetwork = window.ethereum.networkVersion 
+  avaxFujiTestnet: string = 'https://api.avax-test.network/ext/bc/C/rpc'
+  constructor(
+    private metamaskService: MetamaskService,
+    private cd: ChangeDetectorRef
+  ) {}
+ async ngOnInit() {
+    this.connectToWallet();
+    this.metamaskService.addWalletChangeListener((accounts: string[]) => {
+      this.updateDisplayedWallet(accounts[0]);
+    });
+    await this.metamaskService.addChainChangedListener((chainId) => {
+      this.updateChain()
+    })
+  }
+  ngOnDestroy(): void {
+    this.metamaskService.removeWalletChangeListener((accounts: string[]) => {
+      this.updateDisplayedWallet(accounts[0]);
+    });
+    this.metamaskService.removeChainChangedListener((chainId) => {
+      this.updateChain()
+    })
+  }
 
+  updateDisplayedWallet(account: string): void {
+    this.address = account;
+    this.cd.detectChanges();
+  }
+  updateChain(){
+    this.metamaskService.switchToAvax(this.selectedNetwork)
+    this.cd.detectChanges()
+
+  }
+  public connectToWallet() {
+    this.metamaskService
+      .connectToMetamask()
+      .then((address: string) => {
+        this.address = address;
+        this.cd.detectChanges();
+        // this.metamaskService.onNetworkChanged()
+        // .catch((error:string) => {console.log(error)})
+        // window.ethereum.on('accountsChanged', async() => {
+        //   try {
+        //     const address = await this.metamaskService.onAccountsChanged();
+        //     this.address = address;
+        //   } catch(error) {
+        //     console.log(error)
+        //   }
+        // })
+        // window.ethereum.on('chainChanged', () => {
+        //   this.cd.detectChanges()
+        // });
+      })
+      .catch((error: string) => {
+        console.log(error);
+      });
+  }
 }
